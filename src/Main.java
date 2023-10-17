1ded385de87ace1aff2327b5655b0e04be7dfb8c
VGlyva <135941893+VGlyva@users.noreply.github.com>
2023-10-17 23:08:42 +0700

    Мой первый коммит

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Set<Integer> set = new LinkedHashSet<>(nums);
+        List<Integer> numbers = new ArrayList<>(set);
+        TestService.printEven(nums);
+        System.out.println();
+        TestService.printEvenNoRepeat(numbers);
+        System.out.println();
+        TestService.printWords();
+        TestService.printWordsUnique();
+
+    }
+
+}
---
+++ src/TestService.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class TestService {
+    public static List<String> words = new ArrayList<>(List.of("abc", "abc", "bcb", "bcb", "e"));
+
+    public static void printEven(List<Integer> nums) {
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static void printEvenNoRepeat(List<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 == 0) {
+                Arrays.sort(new Integer[]{nums.get(i)});
+                System.out.print(nums.get(i) + " ");
+            }
+        }
+    }
+
+    public static void printWords() {
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(uniqueWords);
+    }
+
+    public static void printWordsUnique() {
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(words.size() - uniqueWords.size());
+    }
+}
+
+
